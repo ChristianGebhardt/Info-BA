77d046187d9e7adc05d6c0217f2a0736b6795013
ChristianGebhardt <gebhardt.christian@campus.lmu.de>
2016-08-23 20:55:32 +0200

    Further edge

--- BA/src/Main.java
+++ BA/src/Main.java
@@ -38,6 +38,7 @@ public class Main {
 	    myNetwork.addEdge(1,3,4);
 	    myNetwork.addEdge(1,4,1);
 	    myNetwork.addEdge(2,4,4);
+	    myNetwork.addEdge(2,5,1);
 	    myNetwork.addEdge(3,2,1);
 	    myNetwork.addEdge(3,5,2);
 	    myNetwork.addEdge(4,5,6);
